a74e765e1e464badab8242d30e386cb6fb016206
liightoh <chekkalhaissam@gmail.com>
2016-06-13 11:04:25 +0200

    okai

--- myapp/www/php/insert.php
+++ myapp/www/php/insert.php
@@ -5,4 +5,4 @@ $log = mysql_real_escape_string($data->log);
 $pass = mysql_real_escape_string($data->pass);
 mysqli::__construct("localhost","root","");
 mysql_select_db("company");
-mysql_query("INSERT INTO membres(`login`, `password`)VALUES('".$log."','".$pass."')");
+mysql_query("INSERT INTO membres(`login`, `password`)VALUES('".$log."','".$pass."')");sasa
