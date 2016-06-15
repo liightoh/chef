0ff8a239b1351061cc029af04f12ab26abee7680
HBoughid <hamidboughida@hotmail.fr>
2016-06-15 02:27:40 +0200

    Recettes ajouté. Login enlevé

--- myapp/www/js/app.js
+++ myapp/www/js/app.js
@@ -3,30 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'auth0', 'angular-storage', 'angular-jwt'])
-
-    .controller('LoginCtrl', function(store, $scope, $location, auth) {
-      $scope.login = function() {
-        auth.signin({
-          authParams: {
-            scope: 'openid offline_access',
-            device: 'Mobile device'
-          }
-        }, function(profile, token, accessToken, state, refreshToken) {
-          // Success callback
-          store.set('profile', profile);
-          store.set('token', token);
-          store.set('refreshToken', refreshToken);
-          $location.path('/home');
-        }, function() {
-          // Error callback
-        });
-      }
-    })
+angular.module('starter', ['ionic'])
 
 
 
-.run(function($ionicPlatform, $rootScope, auth, store, jwtHelper, $location) {
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
@@ -44,55 +25,10 @@ angular.module('starter', ['ionic', 'auth0', 'angular-storage', 'angular-jwt'])
       StatusBar.styleDefault();
     }
   });
-  auth.hookEvents();
-
-  var refreshingToken = null;
-  $rootScope.$on('$locationChangeStart', function() {
-    var token = store.get('token');
-    var refreshToken = store.get('refreshToken');
-    if (token) {
-      if (!jwtHelper.isTokenExpired(token)) {
-        if (!auth.isAuthenticated) {
-          auth.authenticate(store.get('profile'), token);
-        }
-      }else {
-        if (refreshToken) {
-          if (refreshingToken === null) {
-            refreshingToken = auth.refreshIdToken(refreshToken).then(function(idToken) {
-              store.set('token', idToken);
-              auth.authenticate(store.get('profile'), idToken);
-            }).finally(function() {
-              refreshingToken = null;
-            });
-          }
-          return refreshingToken;
-        } else {
-          $location.path('login');// Notice: this url must be the one defined
-        }
-      }}})
 })
 
 
-.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider, jwtInterceptorProvider) {
-
-  jwtInterceptorProvider.tokenGetter = function(store, jwtHelper, auth) {
-    var idToken = store.get('token');
-    var refreshToken = store.get('refreshToken');
-    // If no token return null
-    if (!idToken || !refreshToken) {
-      return null;
-    }
-    if (jwtHelper.isTokenExpired(idToken)) {
-      return auth.refreshIdToken(refreshToken).then(function(idToken) {
-        store.set('token', idToken);
-        return idToken;
-      });
-    } else {
-      return idToken;
-    }
-  }
-
-  $httpProvider.interceptors.push('jwtInterceptor');
+.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider.state('home', {
     url:'/home',
@@ -104,48 +40,78 @@ angular.module('starter', ['ionic', 'auth0', 'angular-storage', 'angular-jwt'])
     templateUrl:'templates/entree.html',
   })
 
+  $stateProvider.state('recettequiche', {
+    url:'/recettequiche',
+    templateUrl:'templates/recettequiche.html',
+  })
+
+  $stateProvider.state('recettechevre', {
+    url:'/recettechevre',
+    templateUrl:'templates/recettechevre.html',
+  })
+
+  $stateProvider.state('recettesoupe', {
+    url:'/recettesoupe',
+    templateUrl:'templates/recettesoupe.html',
+  })
+
   $stateProvider.state('platchaud', {
     url:'/platchaud',
     templateUrl:'templates/platchaud.html',
   }),
+      $stateProvider.state('recettefilet', {
+        url:'/recettefilet',
+        templateUrl:'templates/recettefilet.html',
+      })
+
+  $stateProvider.state('recettebourguignon', {
+    url:'/recettebourguignon',
+    templateUrl:'templates/recettebourguignon.html',
+  })
+
+  $stateProvider.state('recettegratin', {
+    url:'/recettegratin',
+    templateUrl:'templates/recettegratin.html',
+  })
 
   $stateProvider.state('platfroid', {
     url:'/platfroid',
     templateUrl:'templates/platfroid.html',
   }),
 
+      $stateProvider.state('recettesaladepate', {
+        url:'/recettesaladepate',
+        templateUrl:'templates/recettesaladepate.html',
+      })
+
+  $stateProvider.state('recettesaladeriz', {
+    url:'/recettesaladeriz',
+    templateUrl:'templates/recettesaladeriz.html',
+  })
+
+  $stateProvider.state('recettegaspacho', {
+    url:'/recettegaspacho',
+    templateUrl:'templates/recettegaspacho.html',
+  })
+
   $stateProvider.state('patisserie', {
     url:'/patisserie',
     templateUrl:'templates/patisserie.html',
-    controller:'PatisserieCtrl',
   }),
 
-  $stateProvider.state('recette', {
-    url:'/recette',
+  $stateProvider.state('recettecrumble', {
+    url:'/recettecrumble',
     templateUrl:'templates/recette.html',
   })
-      .state('login', { // Notice: this state name matches the loginState property value to set in authProvider.init({...}) below...
-        url: '/login',
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl',
-      })
 
-      .state('userInfo', {
-        url: '/userInfo',
-        templateUrl: 'templates/user-info.html',
-        controller: 'UserInfoCtrl',
-        data: {
-          // This tells Auth0 that this state requires the user to be logged in.
-          // If the user isn't logged in and he tries to access this state
-          // he'll be redirected to the login page
-          requiresLogin: true
-        }
-      })
+  $stateProvider.state('recettemeringue', {
+    url:'/recettemeringue',
+    templateUrl:'templates/recettemeringue.html',
+  })
 
-  authProvider.init({
-    domain: 'haissam.eu.auth0.com',
-    clientID: 'MG3gKHQxZG5nbzvzlXryXiIfK6W3hnHI',
-    loginState: 'login' // This is the name of the state where you'll show the login, which is defined above...
+  $stateProvider.state('recettegateauchoco', {
+    url:'/recettegateauchoco',
+    templateUrl:'templates/recettegateauchoco.html',
   })
 
   $stateProvider.state('settings', {
@@ -154,32 +120,20 @@ angular.module('starter', ['ionic', 'auth0', 'angular-storage', 'angular-jwt'])
   })
 
 
-
-
-
     /*page d'acceuil */
 
-  $urlRouterProvider.otherwise('/login')
+  $urlRouterProvider.otherwise('home')
 
 
 })
-    .run(function(auth) {
-      // This hooks all auth events to check everything as soon as the app starts
-      auth.hookEvents();
-    })
 
+    .controller('LoginCtrl', function($scope, $state, $http) {
 
-    .controller('PatisserieCtrl', function ($scope){
-
-    })
-
-.controller('UserInfoCtrl', function($scope, auth, store) {
-  $scope.auth = auth;
-  $scope.logout = function() {
-    auth.signout();
-    store.remove('profile');
-    store.remove('token');
-  }
-});
+      $scope.insertdata=function(){
+        $http.post(insert.php,{'log':$scope.log,'pass':$scope.pass})
+            .success(function(data,status,header,config){
+              console.log("data inserted  successfully")
+            });
+      }})
 
 
